39925bddec014712b53e9962c1e609caf1edeee5
Rafiul-Hossain <rafiulhossain4393@gmail.com>
2024-07-05 00:27:36 +0600

    Initial commit

---
+++ Hotel Management System/LoginForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Hotel_Management_System
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox_username = new Guna.UI2.WinForms.Guna2TextBox();
+            this.TextBox_password = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label_exit = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Button_login = new Guna.UI2.WinForms.Guna2Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox_username
+            // 
+            this.TextBox_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TextBox_username.DefaultText = "";
+            this.TextBox_username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TextBox_username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TextBox_username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBox_username.DisabledState.Parent = this.TextBox_username;
+            this.TextBox_username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBox_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBox_username.FocusedState.Parent = this.TextBox_username;
+            this.TextBox_username.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TextBox_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBox_username.HoverState.Parent = this.TextBox_username;
+            this.TextBox_username.Location = new System.Drawing.Point(38, 201);
+            this.TextBox_username.Name = "TextBox_username";
+            this.TextBox_username.PasswordChar = '\0';
+            this.TextBox_username.PlaceholderText = "";
+            this.TextBox_username.SelectedText = "";
+            this.TextBox_username.ShadowDecoration.Parent = this.TextBox_username;
+            this.TextBox_username.Size = new System.Drawing.Size(240, 36);
+            this.TextBox_username.TabIndex = 1;
+            // 
+            // TextBox_password
+            // 
+            this.TextBox_password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TextBox_password.DefaultText = "";
+            this.TextBox_password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TextBox_password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TextBox_password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBox_password.DisabledState.Parent = this.TextBox_password;
+            this.TextBox_password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBox_password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBox_password.FocusedState.Parent = this.TextBox_password;
+            this.TextBox_password.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TextBox_password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBox_password.HoverState.Parent = this.TextBox_password;
+            this.TextBox_password.Location = new System.Drawing.Point(38, 296);
+            this.TextBox_password.Name = "TextBox_password";
+            this.TextBox_password.PasswordChar = '\0';
+            this.TextBox_password.PlaceholderText = "";
+            this.TextBox_password.SelectedText = "";
+            this.TextBox_password.ShadowDecoration.Parent = this.TextBox_password;
+            this.TextBox_password.Size = new System.Drawing.Size(240, 36);
+            this.TextBox_password.TabIndex = 1;
+            this.TextBox_password.UseSystemPasswordChar = true;
+            // 
+            // label_exit
+            // 
+            this.label_exit.AutoSize = true;
+            this.label_exit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_exit.ForeColor = System.Drawing.Color.Red;
+            this.label_exit.Location = new System.Drawing.Point(290, 0);
+            this.label_exit.Name = "label_exit";
+            this.label_exit.Size = new System.Drawing.Size(23, 25);
+            this.label_exit.TabIndex = 3;
+            this.label_exit.Text = "X";
+            this.label_exit.Click += new System.EventHandler(this.label_exit_Click);
+            this.label_exit.MouseEnter += new System.EventHandler(this.label_exit_MouseEnter);
+            this.label_exit.MouseLeave += new System.EventHandler(this.label_exit_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Display", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(84, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "User Login";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Button_login
+            // 
+            this.Button_login.BackColor = System.Drawing.Color.AliceBlue;
+            this.Button_login.CheckedState.Parent = this.Button_login;
+            this.Button_login.CustomImages.Parent = this.Button_login;
+            this.Button_login.FillColor = System.Drawing.Color.RoyalBlue;
+            this.Button_login.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_login.ForeColor = System.Drawing.Color.White;
+            this.Button_login.HoverState.Parent = this.Button_login;
+            this.Button_login.Image = global::Hotel_Management_System.Properties.Resources.login_64px;
+            this.Button_login.Location = new System.Drawing.Point(22, 371);
+            this.Button_login.Name = "Button_login";
+            this.Button_login.ShadowDecoration.Parent = this.Button_login;
+            this.Button_login.Size = new System.Drawing.Size(278, 45);
+            this.Button_login.TabIndex = 2;
+            this.Button_login.Text = "Login";
+            this.Button_login.Click += new System.EventHandler(this.Button_login_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoEllipsis = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(36, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Username";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoEllipsis = true;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(36, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(315, 500);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_exit);
+            this.Controls.Add(this.Button_login);
+            this.Controls.Add(this.TextBox_password);
+            this.Controls.Add(this.TextBox_username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2TextBox TextBox_username;
+        private Guna.UI2.WinForms.Guna2TextBox TextBox_password;
+        private Guna.UI2.WinForms.Guna2Button Button_login;
+        private System.Windows.Forms.Label label_exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
